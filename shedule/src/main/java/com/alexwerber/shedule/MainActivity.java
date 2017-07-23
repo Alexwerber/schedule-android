d70834a8c8b15037fddd748777be7c5b110cec71
Alexwerber <19751975s@gmail.com>
2017-07-23 17:41:05 +0700

    Tab Layout

--- shedule/src/main/java/com/alexwerber/shedule/MainActivity.java
+++ shedule/src/main/java/com/alexwerber/shedule/MainActivity.java
@@ -1,23 +1,13 @@
-package com.alexwerber.scheduleandroid;
+package com.alexwerber.shedule;
 
-import android.support.design.widget.TabLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TabLayout tabLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        ActionBar actionBar = getSupportActionBar();
-        //actionBar.setDisplayHomeAsUpEnabled(true);
-
     }
-
 }
